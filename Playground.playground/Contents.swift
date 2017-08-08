1faade6a1e5df5c34abbb02e7a59da7c9672fc99
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-08-08 16:05:29 +0900

    Implement Parameters struct

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,5 +1,10 @@
 //: Playground - noun: a place where people can play
 
 import Cocoa
+import SwarmChemistry
 
 var str = "Hello, playground"
+
+let params = (0..<8).map { Double($0) }
+
+print(SwarmChemistry.Parameters.init(params))
---
+++ SwarmChemistry/Parameters.swift
@@ -0,0 +1,57 @@
+//
+//  Agent.swift
+//  SwarmChemistry
+//
+//  Created by mitsuyoshi.yamazaki on 2017/08/08.
+//  Copyright © 2017年 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import Foundation
+
+public struct Parameters {
+  public typealias Value = Double
+  
+  let neighborhoodRadius: Value
+  static let neighborhoodRadiusMax = 300.0
+  
+  let normalSpeed: Value
+  static let normalSpeedMax = 20.0
+  
+  let maxSpeed: Value
+  static let maxSpeedMax = 40.0
+  
+  let cohesiveForce: Value
+  static let cohesiveForceMax = 1.0
+  
+  let aligningForce: Value
+  static let aligningForceMax = 1.0
+  
+  let separatingForce: Value
+  static let separatingForceMax = 100.0
+  
+  let probabilityOfRandomSteering: Value
+  static let probabilityOfRandomSteeringMax = 0.5
+  
+  let tendencyOfPacekeeping: Value
+  static let tendencyOfPacekeepingMax = 1.0
+  
+  private static let numberOfParameters = 8
+  
+  public init?(_ parameters: [Value]) {
+    guard parameters.count == type(of: self).numberOfParameters else {
+      return nil
+    }
+    
+    // should validate each values?
+    neighborhoodRadius          = parameters[0]
+    normalSpeed                 = parameters[1]
+    maxSpeed                    = parameters[2]
+    cohesiveForce               = parameters[3]
+    aligningForce               = parameters[4]
+    separatingForce             = parameters[5]
+    probabilityOfRandomSteering = parameters[6]
+    tendencyOfPacekeeping       = parameters[7]
+  }
+}
+
+
