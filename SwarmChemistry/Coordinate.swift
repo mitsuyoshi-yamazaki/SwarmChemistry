36bfddd3918d723aa93442986fe61f74fa1b6298
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-08-08 17:09:27 +0900

    Export Coordinate to file

---
+++ SwarmChemistry/Coordinate.swift
@@ -0,0 +1,27 @@
+//
+//  Coordinate.swift
+//  SwarmChemistry
+//
+//  Created by mitsuyoshi.yamazaki on 2017/08/08.
+//  Copyright © 2017年 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import Foundation
+
+public struct Coordinate {
+  
+  let x: Value
+  let y: Value
+  //  let z: Double // TODO: make it 3D
+}
+
+extension Coordinate {
+  static let zero = Coordinate(0.0, 0.0)
+}
+
+extension Coordinate {
+  public init(_ x: Value, _ y: Value) {
+    self.x = x
+    self.y = y
+  }
+}
--- SwarmChemistry/Utility.swift
+++ SwarmChemistry/Utility.swift
@@ -10,24 +10,6 @@ import Foundation
 
 public typealias Value = Double
 
-public struct Coordinate {
-  
-  let x: Value
-  let y: Value
-  //  let z: Double // TODO: make it 3D
-}
-
-extension Coordinate {
-  static let zero = Coordinate(0.0, 0.0)
-}
-
-extension Coordinate {
-  public init(_ x: Value, _ y: Value) {
-    self.x = x
-    self.y = y
-  }
-}
-
 internal struct Log {
   private enum Level {
     case debug
