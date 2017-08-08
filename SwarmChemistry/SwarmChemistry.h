de530022aa2b9e078321c4af7952bfbe30c407a4
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-08-08 15:25:55 +0900

    Add targets for iOS and Mac

---
+++ SwarmChemistry/SwarmChemistry.h
@@ -0,0 +1,19 @@
+//
+//  SwarmChemistry.h
+//  SwarmChemistry
+//
+//  Created by mitsuyoshi.yamazaki on 2017/08/08.
+//  Copyright © 2017年 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwarmChemistry.
+FOUNDATION_EXPORT double SwarmChemistryVersionNumber;
+
+//! Project version string for SwarmChemistry.
+FOUNDATION_EXPORT const unsigned char SwarmChemistryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwarmChemistry/PublicHeader.h>
+
+
