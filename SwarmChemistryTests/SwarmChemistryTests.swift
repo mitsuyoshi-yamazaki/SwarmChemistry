5a7b2250a19b96b4b10a7eeeb2ecea2e1b517ddf
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-08-09 14:20:03 +0900

    Add test targets for iOS and Mac

---
+++ SwarmChemistryTests/SwarmChemistryTests.swift
@@ -0,0 +1,35 @@
+//
+//  SwarmChemistryTests_iOS.swift
+//  SwarmChemistryTests_iOS
+//
+//  Created by mitsuyoshi.yamazaki on 2017/08/09.
+//  Copyright © 2017年 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import XCTest
+
+class SwarmChemistryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
