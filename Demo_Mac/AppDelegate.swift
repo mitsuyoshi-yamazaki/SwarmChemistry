7c3523b5e90f3f29b128e813f16689af46b57b04
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-08-09 15:37:49 +0900

    Add mac demo app target

---
+++ Demo_Mac/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Demo_Mac
+//
+//  Created by mitsuyoshi.yamazaki on 2017/08/09.
+//  Copyright © 2017年 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+}
---
+++ Demo_Mac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo_Mac
+//
+//  Created by mitsuyoshi.yamazaki on 2017/08/09.
+//  Copyright © 2017年 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+}
+
