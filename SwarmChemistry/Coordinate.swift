1c95f92627167046ef8d3f4389f00c8f296f4966
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-08-09 14:43:45 +0900

    Add CoordinateTests

--- SwarmChemistry/Coordinate.swift
+++ SwarmChemistry/Coordinate.swift
@@ -15,22 +15,49 @@ public struct Coordinate {
   //  let z: Double // TODO: make it 3D
 }
 
-extension Coordinate {
+public extension Coordinate {
   static let zero = Coordinate(0.0, 0.0)
   
   func distance(_ other: Coordinate) -> Value {
-    return max(hypot(x - other.x, y - other.y), 0.0001)
+    return max(hypot(x - other.x, y - other.y), 0.0001) // TODO: consider if 0.0001 literal needed
+  }
+  
+  func fit(to coordinate: Coordinate) -> Coordinate {
+    let x: Value
+    if self.x > coordinate.x {
+      x = self.x - (floor(self.x / coordinate.x) * coordinate.x)
+    } else if self.x < 0.0 {
+      x = self.x + (floor((-self.x + coordinate.x) / coordinate.x) * coordinate.x)
+    } else {
+      x = self.x
+    }
+
+    let y: Value
+    if self.y > coordinate.y {
+      y = self.y - (floor(self.y / coordinate.y) * coordinate.y)
+    } else if self.y < 0.0 {
+      y = self.y + (floor((-self.y + coordinate.y) / coordinate.y) * coordinate.y)
+    } else {
+      y = self.y
+    }
+
+    return Coordinate(x, y)
   }
 }
 
-extension Coordinate {
+public extension Coordinate {
   public init(_ x: Value, _ y: Value) {
     self.x = x
     self.y = y
   }
+  
+  public init(_ x: Int, _ y: Int) {
+    self.x = Value(x)
+    self.y = Value(y)
+  }
 }
 
-extension Coordinate {
+public extension Coordinate {
   static func +(lhs: Coordinate, rhs: Coordinate) -> Coordinate {
     return Coordinate(lhs.x + rhs.x, lhs.y + rhs.y)
   }
@@ -51,5 +78,11 @@ extension Coordinate {
 extension Coordinate: CustomStringConvertible {
   public var description: String {
     return "(\(x), \(y))"
+  }
+}
+
+extension Coordinate: Equatable {
+  public static func ==(lhs: Coordinate, rhs: Coordinate) -> Bool {
+    return (lhs.x == rhs.x) && (lhs.y == rhs.y)
   }
 }
---
+++ SwarmChemistryTests/CoordinateTests.swift
@@ -0,0 +1,48 @@
+//
+//  CoordinateTests.swift
+//  SwarmChemistry
+//
+//  Created by mitsuyoshi.yamazaki on 2017/08/09.
+//  Copyright © 2017年 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import XCTest
+@testable import SwarmChemistry
+
+class CoordinateTests: XCTestCase {
+
+  func test_zero() {
+    let zero = Coordinate.zero
+    
+    XCTAssert(zero == Coordinate(0, 0))
+  }
+  
+  func test_init() {
+    XCTAssert(Coordinate(12.0, 12.0) == Coordinate(12, 12))
+  }
+  
+  func test_distance() {
+    let zero = Coordinate.zero
+    
+    XCTAssert(zero.distance(Coordinate(3, 4)) == 5)
+    XCTAssert(zero.distance(Coordinate(-3, -4)) == 5)
+    XCTAssert(zero.distance(Coordinate(00001, 0)) == 0001)
+  }
+  
+  func test_fit() {
+    let positive = Coordinate(105, 105)
+    
+    XCTAssert(positive.fit(to: Coordinate(100, 100)) == Coordinate(5, 5))
+    XCTAssert(positive.fit(to: Coordinate(10, 10)) == Coordinate(5, 5))
+    XCTAssert(positive.fit(to: Coordinate(200, 200)) == positive)
+    
+    let negative = Coordinate(-5, -5)
+    XCTAssert(negative.fit(to: Coordinate(100, 100)) == Coordinate(95, 95))
+    XCTAssert(negative.fit(to: Coordinate(10, 10)) == Coordinate(5, 5))
+  }
+  
+  func test_equality() {
+    XCTAssert(Coordinate(3.2, 1.0) == Coordinate(3.2, 1.0))
+    XCTAssert(Coordinate(3.2, 1.0) != Coordinate(3.2, 1.1))
+  }
+}
