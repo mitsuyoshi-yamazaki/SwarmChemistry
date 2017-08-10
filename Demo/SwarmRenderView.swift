71ea902781bc9dba01be8ede11b1ddd68ad9de80
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2017-08-10 23:25:01 +0900

    Make SwarmRenderView independent from platform

--- Demo/SwarmRenderView.swift
+++ Demo/SwarmRenderView.swift
@@ -7,23 +7,55 @@
 //
 
 #if os(iOS) || os(watchOS) || os(tvOS)
-  
   import UIKit
   typealias View = UIView
 #elseif os(macOS)
-  
   import Cocoa
   typealias View = NSView
 #endif
 
+import SwarmChemistry
+
 class SwarmRenderView: View {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+  var population: Population?
+  var cellSize: CGFloat = 16.0
 
+  #if os(iOS) || os(watchOS) || os(tvOS)
+  override func draw(_ rect: CGRect) {
+    let context = UIGraphicsGetCurrentContext()!
+    draw(with: context)
+  }
+  #elseif os(macOS)
+  override func draw(_ dirtyRect: NSRect) {
+    guard let context = NSGraphicsContext.current()?.cgContext else {
+      fatalError()
+    }
+    draw(with: context)
+  }
+  #endif
+  
+  private func draw(with context: CGContext) {
+
+    context.setFillColor(Color(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor)
+    context.fill(bounds)
+    
+    guard let population = population else {
+      return
+    }
+    
+    let fieldWidth = CGFloat(population.fieldSize.x)
+    let fieldHeight = CGFloat(population.fieldSize.y)
+    let multiplier = min(frame.width / fieldWidth, frame.height / fieldHeight)
+    let size = cellSize * multiplier
+    
+    context.setFillColor(Color.white.cgColor)
+    context.fill(.init(x: 0.0, y: 0.0, width: fieldWidth * multiplier, height: fieldHeight * multiplier))
+    
+    for individual in population.population {
+      
+      individual.genome.color.setFill()
+      context.fillEllipse(in: CGRect(x: CGFloat(individual.position.x) * multiplier, y: CGFloat(individual.position.y) * multiplier, width: size, height: size))
+    }
+  }
 }
--- Demo_Mac/ViewController.swift
+++ Demo_Mac/ViewController.swift
@@ -11,7 +11,7 @@ import SwarmChemistry
 
 class ViewController: NSViewController {
 
-  @IBOutlet private weak var renderView: RenderView!
+  @IBOutlet private weak var renderView: SwarmRenderView!
   
   private var isRunning = false
   
@@ -68,36 +68,4 @@ class ViewController: NSViewController {
 
 func random() -> Int {
   return Int(arc4random())
-}
-
-class RenderView: NSView {
-  
-  var population: Population?
-  var cellSize: CGFloat = 16.0
-
-  override func draw(_ dirtyRect: NSRect) {
-    guard let context = NSGraphicsContext.current()?.cgContext else {
-      fatalError()
-    }
-    context.setFillColor(NSColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor)
-    context.fill(bounds)
-
-    guard let population = population else {
-      return
-    }
-
-    let fieldWidth = CGFloat(population.fieldSize.x)
-    let fieldHeight = CGFloat(population.fieldSize.y)
-    let multiplier = min(frame.width / fieldWidth, frame.height / fieldHeight)
-    let size = cellSize * multiplier
-    
-    context.setFillColor(NSColor.white.cgColor)
-    context.fill(.init(x: 0.0, y: 0.0, width: fieldWidth * multiplier, height: fieldHeight * multiplier))
-    
-    for individual in population.population {
-      
-      individual.genome.color.setFill()
-      context.fillEllipse(in: CGRect(x: CGFloat(individual.position.x) * multiplier, y: CGFloat(individual.position.y) * multiplier, width: size, height: size))
-    }
-  }
 }
