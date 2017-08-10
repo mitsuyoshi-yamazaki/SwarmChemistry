1ff0750d3a283d2a24fefdf556563fe1e0e8252d
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-08-10 10:48:04 +0900

    Add Recipe struct

---
+++ SwarmChemistry/Recipe.swift
@@ -0,0 +1,13 @@
+//
+//  Recipe.swift
+//  SwarmChemistry
+//
+//  Created by mitsuyoshi.yamazaki on 2017/08/10.
+//  Copyright © 2017年 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+  
+}
