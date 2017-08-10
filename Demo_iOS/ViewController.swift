91690ad944b81cfc65cb6a5d08e8ffa6cc6ea25a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2017-08-10 23:10:31 +0900

    Add iOS target

---
+++ Demo_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo_iOS
+//
+//  Created by Yamazaki Mitsuyoshi on 2017/08/10.
+//  Copyright © 2017 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
