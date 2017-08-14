b424dc1d9ab3dc89fe2f3aa60a3a5b6b44bbfacb
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2017-08-14 21:16:24 +0900

    Add cancel button

---
+++ Demo_iOS/Utility.swift
@@ -0,0 +1,15 @@
+//
+//  Utility.swift
+//  SwarmChemistry
+//
+//  Created by Yamazaki Mitsuyoshi on 2017/08/14.
+//  Copyright © 2017 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+  @IBAction func dismiss(sender: AnyObject!) {
+    dismiss(animated: true, completion: nil)
+  }
+}
