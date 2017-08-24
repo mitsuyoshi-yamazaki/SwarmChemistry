20e0b291b8388dd740fae21990f35afa0ed1dd61
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-08-24 18:48:37 +0900

    Implement close ConfigureWindow function

--- Demo_ScreenSaver/ConfigureWindow.swift
+++ Demo_ScreenSaver/ConfigureWindow.swift
@@ -7,7 +7,23 @@
 //
 
 import Cocoa
+import SwarmChemistry
+
+protocol ConfigureWindowDelegate: class {
+  func configureWindow(_ window: ConfigureWindow, didSelect recipe: Recipe)
+}
 
 final class ConfigureWindow: NSWindow, IBInstantiatable {
 
+  weak var configureWindowDelegate: ConfigureWindowDelegate?
+  
+  @IBAction func ok(sender: AnyObject!) {
+    NSApp.endSheet(self)
+//    endSheet(self)  // Doesn't work
+  }
+  
+  @IBAction func cancel(sender: AnyObject!) {
+    NSApp.endSheet(self)
+    //    endSheet(self)  // Doesn't work
+  }
 }
