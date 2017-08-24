f30a9a5d0dc4174cfb44ace151a510d03c4cea94
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-08-24 19:08:20 +0900

    Show recipe list on the sheet

--- Demo_ScreenSaver/ConfigureWindow.swift
+++ Demo_ScreenSaver/ConfigureWindow.swift
@@ -16,7 +16,11 @@ protocol ConfigureWindowDelegate: class {
 final class ConfigureWindow: NSWindow, IBInstantiatable {
 
   weak var configureWindowDelegate: ConfigureWindowDelegate?
+  var currentRecipe: Recipe?
+
+  fileprivate let recipeList = Recipe.presetRecipes
   
+  // MARK: -
   @IBAction func ok(sender: AnyObject!) {
     NSApp.endSheet(self)
 //    endSheet(self)  // Doesn't work
@@ -25,5 +29,32 @@ final class ConfigureWindow: NSWindow, IBInstantiatable {
   @IBAction func cancel(sender: AnyObject!) {
     NSApp.endSheet(self)
     //    endSheet(self)  // Doesn't work
+  }
+}
+
+extension ConfigureWindow: NSTableViewDataSource {
+  private enum ColumnIdentifier: String {
+    case selected   = "Selected"
+    case recipeName = "RecipeName"
+  }
+  
+  func numberOfRows(in tableView: NSTableView) -> Int {
+    return recipeList.count
+  }
+  
+  func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+    guard let tableColumn = tableColumn else {
+      Swift.print("No tableColumn specified")
+      return nil
+    }
+    
+    let recipe = recipeList[row]
+    
+    switch ColumnIdentifier.init(rawValue: tableColumn.identifier)! {
+    case .selected:
+      return "✔︎"
+    case .recipeName:
+      return recipe.name
+    }
   }
 }
