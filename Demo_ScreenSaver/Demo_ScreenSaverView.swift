29a4c6a7af7f5ffee5ed4154e0585c54fbc35d0a
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-08-24 20:59:20 +0900

    Implement random recipe for @Home target

--- Demo_ScreenSaver/Demo_ScreenSaverView.swift
+++ Demo_ScreenSaver/Demo_ScreenSaverView.swift
@@ -24,7 +24,14 @@ class Demo_ScreenSaverView: ScreenSaverView {
     return window
   }()
   
+  #if AT_HOME
+  private let fieldSizeMultiplier: CGFloat = 0.15
+  private let steps = 1
+  #else
   private let fieldSizeMultiplier: CGFloat = 0.25
+  private let steps = 10
+  #endif
+  
   private var population = Population.empty()
   private var isRunning = false {
     didSet {
@@ -39,7 +46,11 @@ class Demo_ScreenSaverView: ScreenSaverView {
   
   override var animationTimeInterval: TimeInterval {
     get {
-      return 0.15
+      #if AT_HOME
+        return isPreview ? 0.15 : 5.0
+      #else
+        return 0.15
+      #endif
     }
     set {}
   }
@@ -67,12 +78,21 @@ class Demo_ScreenSaverView: ScreenSaverView {
     let fieldSize = Vector2(width, height)
     let initialArea = Vector2.Rect.init(origin: fieldSize * 0.2, size: fieldSize * 0.6)
     
-    let recipe = selectedRecipe ?? Recipe.jellyFish
-    
-    population = Population.init(recipe,
-                                 numberOfPopulation: 1000,
-                                 fieldSize: fieldSize,
-                                 initialArea: initialArea)
+    #if AT_HOME
+      let numberOfPopulation = isPreview ? 1000 : 5000
+      let recipe = Recipe.random(numberOfGenomes: 20, fieldSize: fieldSize.rect)
+      population = Population.init(recipe,
+                                   numberOfPopulation: numberOfPopulation,
+                                   fieldSize: fieldSize,
+                                   initialArea: initialArea)
+    #else
+      let recipe = selectedRecipe ?? Recipe.jellyFish
+      
+      population = Population.init(recipe,
+                                   numberOfPopulation: 1000,
+                                   fieldSize: fieldSize,
+                                   initialArea: initialArea)
+    #endif
     
     contentView.set(title: population.recipe.name)
     contentView.set(steps: population.steps)
@@ -88,7 +108,7 @@ class Demo_ScreenSaverView: ScreenSaverView {
       return
     }
     DispatchQueue.global().async {
-      self.population.step(10)
+      self.population.step(self.steps)
       self.step()
     }
   }
@@ -122,7 +142,11 @@ class Demo_ScreenSaverView: ScreenSaverView {
   }
   
   override func hasConfigureSheet() -> Bool {
-    return isPreview
+    #if AT_HOME
+      return false
+    #else
+      return isPreview
+    #endif
   }
   
   override func configureSheet() -> NSWindow? {
