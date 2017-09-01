cd95977eccca4a69b7435b8b7849643dc035ec83
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-09-01 20:24:08 +0900

    Use SwarmRenderView in Demo_ScreenSaverView

--- Demo_ScreenSaver/Demo_ScreenSaverView.swift
+++ Demo_ScreenSaver/Demo_ScreenSaverView.swift
@@ -11,8 +11,7 @@ import ScreenSaver
 import SwarmChemistry
 
 // https://developer.apple.com/documentation/screensaver
-// TODO: Change class name & set it to Principal Class in Info.plist
-class Demo_ScreenSaverView: ScreenSaverView {
+class Demo_ScreenSaverView: ScreenSaverView, SwarmRenderer {
   
   private let contentView: ContentView = {
     let view = ContentView.instantiate()
@@ -23,26 +22,6 @@ class Demo_ScreenSaverView: ScreenSaverView {
     let window = ConfigureWindow.instantiate()
     return window
   }()
-  
-  #if AT_HOME
-  private let fieldSizeMultiplier: CGFloat = 0.15
-  private let steps = 1
-  #else
-  private let fieldSizeMultiplier: CGFloat = 0.25
-  private let steps = 10
-  #endif
-  
-  private var population = Population.empty()
-  private var isRunning = false {
-    didSet {
-      guard isRunning != oldValue else {
-        return
-      }
-      if isRunning {
-        step()
-      }
-    }
-  }
   
   fileprivate var recipeID: Int?
   
@@ -57,6 +36,33 @@ class Demo_ScreenSaverView: ScreenSaverView {
     set {}
   }
   
+  // MARK: - SwarmRenderer
+  var renderView: SwarmRenderView! = {
+    let view = SwarmRenderView()
+    return view
+  }()
+  var isRunning = false {
+    didSet {
+      guard isRunning != oldValue else {
+        return
+      }
+      if isRunning {
+        step()
+      }
+    }
+  }
+  var delay: Double {
+    return 0.0
+  }
+  #if AT_HOME
+  private let fieldSizeMultiplier: CGFloat = 0.15
+  var steps = 1
+  #else
+  private let fieldSizeMultiplier: CGFloat = 0.25
+  var steps = 10
+  #endif
+
+  // MARK: -
   required init?(coder: NSCoder) {
     super.init(coder: coder)
     setup()
@@ -68,6 +74,9 @@ class Demo_ScreenSaverView: ScreenSaverView {
   }
   
   private func setup() {
+    renderView.frame = bounds
+    addSubview(renderView)
+    
     contentView.frame = bounds
     addSubview(contentView)
     
@@ -79,6 +88,7 @@ class Demo_ScreenSaverView: ScreenSaverView {
     let height = Int(frame.size.height / fieldSizeMultiplier)
     let fieldSize = Vector2(width, height)
     let initialArea = Vector2.Rect.init(origin: fieldSize * 0.2, size: fieldSize * 0.6)
+    let population: Population
     
     #if AT_HOME
       let numberOfPopulation = isPreview ? 1000 : 5000
@@ -100,48 +110,49 @@ class Demo_ScreenSaverView: ScreenSaverView {
     #endif
     
     contentView.set(steps: population.steps)
+    setupRenderView(with: population)
   }
   
   override func startAnimation() {
     super.startAnimation()
-    isRunning = true
+    resume()
   }
   
-  private func step() {
-    guard isRunning else {
-      return
-    }
-    DispatchQueue.global().async {
-      self.population.step(self.steps)
-      self.step()
-    }
-  }
+//  private func step() {
+//    guard isRunning else {
+//      return
+//    }
+//    DispatchQueue.global().async {
+//      self.population.step(self.steps)
+//      self.step()
+//    }
+//  }
   
   override func stopAnimation() {
     super.stopAnimation()
-    isRunning = false
+    pause()
   }
   
-  override func draw(_ rect: NSRect) {
-    guard let context = NSGraphicsContext.current()?.cgContext else {
-      fatalError()
-    }
-    
-    let fieldWidth = CGFloat(population.fieldSize.x)
-    let fieldHeight = CGFloat(population.fieldSize.y)
-    let size = 20.0 * fieldSizeMultiplier
-    
-    context.setFillColor(Color.white.cgColor)
-    context.fill(NSRect.init(x: 0.0, y: 0.0, width: fieldWidth * fieldSizeMultiplier, height: fieldHeight * fieldSizeMultiplier))
-    
-    for individual in population.population {
-      individual.genome.color.setFill()
-      context.fillEllipse(in: CGRect(x: CGFloat(individual.position.x) * fieldSizeMultiplier, y: CGFloat(individual.position.y) * fieldSizeMultiplier, width: size, height: size))
-    }
-  }
+//  override func draw(_ rect: NSRect) {
+//    guard let context = NSGraphicsContext.current()?.cgContext else {
+//      fatalError()
+//    }
+//    
+//    let fieldWidth = CGFloat(population.fieldSize.x)
+//    let fieldHeight = CGFloat(population.fieldSize.y)
+//    let size = 20.0 * fieldSizeMultiplier
+//    
+//    context.setFillColor(Color.white.cgColor)
+//    context.fill(NSRect.init(x: 0.0, y: 0.0, width: fieldWidth * fieldSizeMultiplier, height: fieldHeight * fieldSizeMultiplier))
+//    
+//    for individual in population.population {
+//      individual.genome.color.setFill()
+//      context.fillEllipse(in: CGRect(x: CGFloat(individual.position.x) * fieldSizeMultiplier, y: CGFloat(individual.position.y) * fieldSizeMultiplier, width: size, height: size))
+//    }
+//  }
   
   override func animateOneFrame() {
-    contentView.set(steps: population.steps)
+    contentView.set(steps: renderView.population.steps)
     setNeedsDisplay(bounds)
   }
   
@@ -158,7 +169,7 @@ class Demo_ScreenSaverView: ScreenSaverView {
       return nil
     }
     configureWindow.configureWindowDelegate = self
-    configureWindow.selectedRecipe = population.recipe
+    configureWindow.selectedRecipe = renderView.population.recipe
     
     return configureWindow
   }
